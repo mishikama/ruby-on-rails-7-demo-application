b55c2aff2473c0351a90e8c6eb28be6f10ae0ab8
Michael <michael@mail.local>
2024-10-31 16:52:14 +0300

    off config.host in environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@ require "active_support/core_ext/integer/time"
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
-  config.hosts << /[a-z0-9]+\.csb\.app/
+  # config.hosts << /[a-z0-9]+\.csb\.app/
 
   # Settings specified here will take precedence over those in config/application.rb.
 
