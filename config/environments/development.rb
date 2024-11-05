76700ba2b82bf532fad57d1774f5072c8d1c973b
Michael <michael@mail.local>
2024-11-05 11:54:19 +0300

    add allowed host in env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   
   config.hosts << /[a-z0-9-.]+\.csb\.app/
+  config.hosts << ENV['ALLOWED_HOST'] if ENV['ALLOWED_HOST'].present?
   
   # Settings specified here will take precedence over those in config/application.rb.
 
